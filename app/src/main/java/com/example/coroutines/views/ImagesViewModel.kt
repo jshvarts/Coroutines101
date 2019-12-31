1865b3eb79d780ffe7b06abd012568b77eddf77e
James Shvarts <jshvarts@gmail.com>
2019-12-31 13:40:45 -0500

    Use try/catch to handle exceptions in UI

--- app/src/main/java/com/example/coroutines/views/ImagesViewModel.kt
+++ app/src/main/java/com/example/coroutines/views/ImagesViewModel.kt
@@ -6,7 +6,6 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.coroutines.repository.ImageRepository
-import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.launch
 import timber.log.Timber
 import javax.inject.Inject
@@ -19,25 +18,26 @@ class ImagesViewModel @Inject constructor(private val imageRepository: ImageRepo
   val isError: LiveData<Boolean>
     get() = _isError
 
-  private val exceptionHandler = CoroutineExceptionHandler { _, throwable ->
-    Timber.e(throwable, "Error occurred downloading images.")
-    _isError.value = true
-  }
-
   private val _images = MutableLiveData<Pair<Bitmap, Bitmap>>()
 
   val images: LiveData<Pair<Bitmap, Bitmap>>
     get() = _images
 
   fun lookupImages() {
-    viewModelScope.launch(exceptionHandler) {
-      val image1 =
-        imageRepository.downloadImage("photo-1577711456630-1daf4a628e46?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjEwODczNX0")
+    viewModelScope.launch {
+      try {
+        val image1 =
+          imageRepository.downloadImage("photo-1577711456630-1daf4a628e46?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjEwODczNX0")
 
-      val image2 =
-        imageRepository.downloadImage("photo-1562886877-0be0db6aba84?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjEwODczNX0")
+        val image2 =
+          imageRepository.downloadImage("photo-1562886877-0be0db6aba84?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjEwODczNX0")
 
-      _images.value = Pair(image1, image2)
+        _images.value = Pair(image1, image2)
+
+      } catch (throwable: Throwable) {
+        Timber.e(throwable, "Error occurred downloading images.")
+        _isError.value = true
+      }
     }
   }
 }
