ba9adcd3a4a1ebfe55738506cc164252394cf550
James Shvarts <jshvarts@gmail.com>
2020-01-14 22:21:14 -0500

    Fix Fragment to ViewModel association

--- app/src/main/java/com/example/coroutines/views/ImagesFragment.kt
+++ app/src/main/java/com/example/coroutines/views/ImagesFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.coroutines.CoroutinesApp
 import com.example.coroutines.R
 import kotlinx.android.synthetic.main.images_fragment.*
@@ -18,7 +20,9 @@ class ImagesFragment : Fragment() {
   }
 
   @Inject
-  lateinit var viewModel: ImagesViewModel
+  lateinit var viewModelFactory: ViewModelProvider.Factory
+
+  private val viewModel: ImagesViewModel by viewModels { viewModelFactory }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
