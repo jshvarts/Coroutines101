56a3eec194caf3fc9e7deb2545dfaf220cae26ce
James Shvarts <jshvarts@gmail.com>
2020-01-14 22:29:49 -0500

    Fix Fragment to ViewModel association

--- app/src/main/java/com/example/coroutines/di/AppComponent.kt
+++ app/src/main/java/com/example/coroutines/di/AppComponent.kt
@@ -7,7 +7,7 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component
+@Component(modules = [ViewModelModule::class])
 interface AppComponent {
   @Component.Factory
   interface Factory {
