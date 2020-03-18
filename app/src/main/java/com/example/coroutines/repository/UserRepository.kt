2678c23b8dfa72894ce2b0160a92874b0d606084
James Shvarts <jshvarts@gmail.com>
2020-03-18 09:35:26 -0400

    comment out code dealing with retries

--- app/src/main/java/com/example/coroutines/repository/UserRepository.kt
+++ app/src/main/java/com/example/coroutines/repository/UserRepository.kt
@@ -3,10 +3,10 @@ package com.example.coroutines.repository
 import com.example.coroutines.domain.UserDetails
 import com.example.coroutines.threading.DispatcherProvider
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.*
-
-private const val DELAY_ONE_SECOND = 1000L
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 
 class UserRepository(
     private val apiService: ApiService,
@@ -18,12 +18,23 @@ class UserRepository(
         return flow {
             val userDetails = apiService.userDetails(login)
             emit(Result.success(userDetails))
-        }.retry(retries = 2) { t ->
-            (t is Exception).also {
-                if (it) delay(DELAY_ONE_SECOND)
-            }
         }
             .catch { emit(Result.failure(it)) }
             .flowOn(dispatchers.io())
     }
+
+// Retry if failed
+//    @ExperimentalCoroutinesApi
+//    fun userDetails(login: String): Flow<Result<UserDetails>> {
+//        return flow {
+//            val userDetails = apiService.userDetails(login)
+//            emit(Result.success(userDetails))
+//        }.retry(retries = 2) { t ->
+//            (t is Exception).also {
+//                if (it) delay(DELAY_ONE_SECOND)
+//            }
+//        }
+//            .catch { emit(Result.failure(it)) }
+//            .flowOn(dispatchers.io())
+//    }
 }
--- app/src/test/java/com/example/coroutines/repository/UserRepositoryTest.kt
+++ app/src/test/java/com/example/coroutines/repository/UserRepositoryTest.kt
@@ -1,27 +1,26 @@
 package com.example.coroutines.repository
 
 import com.example.coroutines.domain.UserDetails
-import com.example.coroutines.threading.CoroutineTestRule
+import com.example.coroutines.threading.TestDispatcherProvider
 import com.nhaarman.mockitokotlin2.doAnswer
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.single
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
-import org.junit.Rule
 import org.junit.Test
 import java.io.IOException
 
 class UserRepositoryTest {
-    @Suppress("ANNOTATION_TARGETS_NON_EXISTENT_ACCESSOR")
-    @get:Rule
-    private val coroutinesTestRule = CoroutineTestRule()
+
+    private val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+    private val testDispatcherProvider = TestDispatcherProvider()
 
     @Test
-    fun `should get user details on success`() = coroutinesTestRule.testDispatcher.runBlockingTest {
+    fun `should get user details on success`() = testDispatcher.runBlockingTest {
         val userDetails = UserDetails(
             login = "someUsername",
             id = 1,
@@ -31,7 +30,7 @@ class UserRepositoryTest {
             onBlocking { userDetails("someUsername") } doReturn userDetails
         }
 
-        val repository = UserRepository(apiService, coroutinesTestRule.testDispatcherProvider)
+        val repository = UserRepository(apiService, testDispatcherProvider)
 
         val flow = repository.userDetails("someUsername")
 
@@ -42,14 +41,14 @@ class UserRepositoryTest {
     }
 
     @Test
-    fun `should get error for user details`() = coroutinesTestRule.testDispatcher.runBlockingTest {
+    fun `should get error for user details`() = testDispatcher.runBlockingTest {
         val apiService = mock<ApiService> {
             onBlocking { userDetails("someUsername") } doAnswer {
                 throw IOException()
             }
         }
 
-        val repository = UserRepository(apiService, coroutinesTestRule.testDispatcherProvider)
+        val repository = UserRepository(apiService, testDispatcherProvider)
 
         val flow = repository.userDetails("someUsername")
 
@@ -59,49 +58,47 @@ class UserRepositoryTest {
     }
 
     @Test
-    fun `should retry with error and retry failed`() =
-        coroutinesTestRule.testDispatcher.runBlockingTest {
-            val apiService = mock<ApiService> {
-                onBlocking { userDetails("someUsername") } doAnswer {
-                    throw IOException()
-                }
-            }
-
-            val repository = UserRepository(apiService, coroutinesTestRule.testDispatcherProvider)
-
-            val flow = repository.userDetails("someUsername")
-            flow.collect { assertTrue("all retries failed with error", it.isFailure) }
-        }
-
-    @Test
-    fun `should retry with error and retry succeeds`() =
-        coroutinesTestRule.testDispatcher.runBlockingTest {
-            // runBlockingTest allows advancing time
-            var throwError = true
-
-            val userDetails = UserDetails(
-                login = "someUsername",
-                id = 1,
-                avatarUrl = "someAvatarUrl"
-            )
-
-            val apiService = mock<ApiService> {
-                onBlocking { userDetails("someUsername") } doAnswer {
-                    if (throwError) throw IOException() else userDetails
-                }
-            }
-
-            val repository = UserRepository(apiService, coroutinesTestRule.testDispatcherProvider)
-
-            pauseDispatcher {
-                val flow = repository.userDetails("someUsername")
-                launch {
-                    flow.collect { assertTrue("retry succeeds", it.isSuccess) }
-                }
-
-                advanceTimeBy(1000L)
-                throwError = false
-                //advanceTimeBy(1000L)
+    fun `should retry with error and retry failed`() = testDispatcher.runBlockingTest {
+        val apiService = mock<ApiService> {
+            onBlocking { userDetails("someUsername") } doAnswer {
+                throw IOException()
             }
         }
+
+        val repository = UserRepository(apiService, testDispatcherProvider)
+
+        val flow = repository.userDetails("someUsername")
+        flow.collect { assertTrue("all retries failed with error", it.isFailure) }
+    }
+
+//    @Test
+//    fun `should retry with error and retry succeeds`() = testDispatcher.runBlockingTest {
+//        // runBlockingTest allows advancing time
+//        var throwError = true
+//
+//        val userDetails = UserDetails(
+//            login = "someUsername",
+//            id = 1,
+//            avatarUrl = "someAvatarUrl"
+//        )
+//
+//        val apiService = mock<ApiService> {
+//            onBlocking { userDetails("someUsername") } doAnswer {
+//                if (throwError) throw IOException() else userDetails
+//            }
+//        }
+//
+//        val repository = UserRepository(apiService, testDispatcherProvider)
+//
+//        pauseDispatcher {
+//            val flow = repository.userDetails("someUsername")
+//            launch {
+//                flow.collect { assertTrue("retry succeeds", it.isSuccess) }
+//            }
+//
+//            advanceTimeBy(1000L)
+//            throwError = false
+//            advanceTimeBy(1000L)
+//        }
+//    }
 }
