86521f9fe7d89f3a3423421b183cdf539919fea2
James Shvarts <jshvarts@gmail.com>
2020-03-18 20:24:44 -0400

    view model success case test

---
+++ app/src/test/java/com/example/coroutines/threading/CoroutineTestRule.kt
@@ -0,0 +1,23 @@
+package com.example.coroutines.threading
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.setMain
+import org.junit.rules.TestWatcher
+import org.junit.runner.Description
+
+class CoroutineTestRule(
+    val dispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+) : TestWatcher() {
+    override fun starting(description: Description?) {
+        super.starting(description)
+        Dispatchers.setMain(dispatcher)
+    }
+
+    override fun finished(description: Description?) {
+        super.finished(description)
+        Dispatchers.resetMain()
+        dispatcher.cleanupTestCoroutines()
+    }
+}
